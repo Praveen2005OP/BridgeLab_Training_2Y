61f646b36bd28c0ad36b15630428bc7c51985fc9
Praveen Kumar Sharma <sharmakarpraveen@gmail.com>
2025-10-18 23:48:37 +0530

    Delete Interface/test.java

--- Interface/test.java
+++
@@ -1,8 +0,0 @@
-package org.example.Functional_Interface;
-
-public class test {
-    public static void main(String[] args) {
-        String password = "Pra@220405";
-        System.out.println("abcdefghijklmnopqrstuvwxyz".contains(password));
-    }
-}
